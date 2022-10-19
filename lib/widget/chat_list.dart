65700490afd6b1e005cc8184e98b1e11535eb020
Dilshad Alam <rayeendilshan@gmail.com>
2022-10-19 22:33:46 +0530

    messageField

--- lib/widget/chat_list.dart
+++ lib/widget/chat_list.dart
@@ -5,17 +5,21 @@ import 'package:whatsapp_clone/widget/my_message_card.dart';
 import 'package:whatsapp_clone/widget/sender_message.dart';
 
 class ChatList extends StatelessWidget {
-  const ChatList({
+  ChatList({
     Key? key,
     required this.name,
     required this.profilePic,
   }) : super(key: key);
   final String name;
   final String profilePic;
+  final TextEditingController _messageController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: true,
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomSheet: _bottomInputField(),
       appBar: AppBar(
         automaticallyImplyLeading: true,
         backgroundColor: appBarColor,
@@ -49,6 +53,107 @@ class ChatList extends StatelessWidget {
             date: messages[index]['time'].toString(),
           );
         },
+      ),
+    );
+  }
+
+  Widget _bottomInputField() {
+    return Container(
+      width: double.infinity,
+      height: 60,
+      color: backgroundColor,
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 3),
+      child: Row(
+        children: [
+          Expanded(
+            child: TextFormField(
+              controller: _messageController,
+              onChanged: (value) {},
+              decoration: InputDecoration(
+                  hintText: "Type a Message",
+                  hintStyle: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 15,
+                    fontWeight: FontWeight.w300,
+                  ),
+                  prefixIcon: IconButton(
+                    icon: const Icon(
+                      Icons.emoji_emotions,
+                      color: Colors.yellowAccent,
+                    ),
+                    onPressed: () {},
+                  ),
+                  suffixIcon: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      IconButton(
+                        style: IconButton.styleFrom(
+                          elevation: 8,
+                          padding: const EdgeInsets.all(0),
+                        ),
+                        icon: const Icon(
+                          Icons.attachment_rounded,
+                          color: Colors.white,
+                        ),
+                        onPressed: () {},
+                      ),
+                      if (_messageController.text.isEmpty)
+                        Row(
+                          children: [
+                            IconButton(
+                              style: IconButton.styleFrom(
+                                elevation: 8,
+                                padding: const EdgeInsets.all(0),
+                              ),
+                              icon: const Icon(
+                                Icons.currency_rupee,
+                                color: Colors.white,
+                              ),
+                              onPressed: () {},
+                            ),
+                            IconButton(
+                              style: IconButton.styleFrom(
+                                elevation: 8,
+                                padding: const EdgeInsets.all(0),
+                              ),
+                              icon: const Icon(
+                                Icons.camera_alt_outlined,
+                                color: Colors.white,
+                              ),
+                              onPressed: () {},
+                            ),
+                          ],
+                        )
+                    ],
+                  ),
+                  enabled: true,
+                  contentPadding: const EdgeInsets.all(7),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                      borderSide: const BorderSide(color: Colors.grey)),
+                  filled: true,
+                  focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                      borderSide: const BorderSide(color: Colors.grey)),
+                  fillColor: Colors.transparent),
+              minLines: 1,
+              maxLines: 100,
+            ),
+          ),
+          const SizedBox(width: 8),
+          CircleAvatar(
+            radius: 23,
+            backgroundColor: messageColor.withOpacity(0.9),
+            child: IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.mic,
+                color: Colors.white,
+              ),
+            ),
+          )
+        ],
       ),
     );
   }

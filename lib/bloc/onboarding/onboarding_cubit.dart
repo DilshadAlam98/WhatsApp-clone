fdaee0f318bc106151a12b00b7153f41f54513e6
Dilshad Alam <rayeendilshan@gmail.com>
2022-12-03 12:49:42 +0530

    feature-auth completed

--- lib/bloc/onboarding/onboarding_cubit.dart
+++ lib/bloc/onboarding/onboarding_cubit.dart
@@ -1,9 +1,13 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:rounded_loading_button/rounded_loading_button.dart';
 import 'package:whatsapp_clone/constant/global_variable.dart';
+import 'package:whatsapp_clone/constant/route_constant.dart';
 import 'package:whatsapp_clone/model/country_model.dart';
 import 'package:whatsapp_clone/repository/onboarding_repo.dart';
+import 'package:whatsapp_clone/utils/dialog_utils.dart';
 
 part 'onboarding_state.dart';
 
@@ -78,15 +82,103 @@ class OnboardingCubit extends Cubit<OnboardingState> {
 
   String? validateForm(String? value) {
     if (value == null || value.isEmpty) {
-      return "Value is Required";
+      return "Field is Required";
     }
 
     return null;
   }
 
-  void verifyYourNumber() async {
-    state.buttonController?.start();
-    emit(state.copyWith(isOtpSent: true, networkState: NetworkState.completed));
-    state.buttonController?.reset();
+  String? validateOtp(String? otp) {
+    if (otp == null || otp.isEmpty) {
+      return "Please enter OTP";
+    }
+    if (otp.length != 6) {
+      return "Please enter 6 digit OTP";
+    }
+    return null;
+  }
+
+  void verifyYourNumber(String phoneNumber, BuildContext context) async {
+    final number = state.countryCode!.code! + phoneNumber;
+    emit(state.copyWith(
+      buttonController: state.buttonController?..start(),
+      isOtpSent: false,
+      errorResponse: null,
+    ));
+    try {
+      await FirebaseAuth.instance.verifyPhoneNumber(
+        phoneNumber: number,
+        verificationCompleted: (phoneAuthCredential) async {},
+        verificationFailed: (error) async {
+          emit(state.copyWith(
+              buttonController: state.buttonController?..reset(),
+              isOtpSent: false,
+              errorResponse: null,
+              verificationId: null));
+          DialogUtils.showInSnackBar(
+            context: context,
+            msg: "Something went wrong, please try later",
+          );
+        },
+        codeSent: (verificationId, forceResendingToken) async {
+          emit(state.copyWith(
+              buttonController: state.buttonController?..reset(),
+              isOtpSent: true,
+              errorResponse: null,
+              verificationId: verificationId));
+          DialogUtils.showToast(
+            msg: "OTP Sent you Successfully",
+          );
+        },
+        codeAutoRetrievalTimeout: (verificationId) async {},
+      );
+    } catch (e) {
+      emit(state.copyWith(
+          buttonController: state.buttonController?..reset(),
+          isOtpSent: false,
+          errorResponse: null,
+          verificationId: null));
+      DialogUtils.showInSnackBar(
+        context: context,
+        msg: "Error occurred, please try later",
+      );
+    }
+  }
+
+  void verifyYourOTP(String otp, BuildContext context) async {
+    emit(state.copyWith(
+      buttonController: state.buttonController?..start(),
+      errorResponse: null,
+      verificationId: null,
+    ));
+    final _credential = PhoneAuthProvider.credential(
+        verificationId: state.verificationId!, smsCode: otp);
+
+    try {
+      final authCredential =
+          await FirebaseAuth.instance.signInWithCredential(_credential);
+
+      if (authCredential.user != null) {
+        //Firebase store stuff
+
+        ///then redirect user
+        Navigator.pushReplacementNamed(
+          context,
+          RouteConstant.mobileScreen,
+        );
+      }
+    } on FirebaseAuthException catch (e) {
+      debugPrint(e.toString());
+      DialogUtils.showInSnackBar(
+        context: context,
+        msg: e.message,
+      );
+      emit(
+        state.copyWith(
+          buttonController: state.buttonController?..reset(),
+          errorResponse: e,
+        ),
+      );
+    }
   }
 }
